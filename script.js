b7417b7fcee98fee0bf210805efc282c035dffb5
anaa142 <nogueira.pinto.ana@escola.pr.gov.br>
2025-11-06 15:25:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button =>[
     button.addEventListener('click', function (){
         const atual = document.querySelector('.ativo');
-        proximoPasso ='passo-' + this.getAttribute
+        proximoPasso ='passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 ])
